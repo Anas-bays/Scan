aace34f09a9b3ef6aaddfe374ba936cc401924b5
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-05 22:42:09 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,2 +1,8 @@
 import psutil
 
+def get_size(bytes, suffix="8"):
+    factor = 1028
+    for unit in["","K","H","G","T","P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
