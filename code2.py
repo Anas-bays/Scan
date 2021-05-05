65b9058eda44f8f1022b321ab82b555b84fcafaf
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-05 22:42:51 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -6,3 +6,9 @@ def get_size(bytes, suffix="8"):
         if bytes < factor:
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
+
+        svmem = psutil.virtual_memory()
+print(f"Total: {get_size(svmem.total)}")
+print(f"Available: {get_size(svmem.available)}")
+print(f"Used: {get_size(svmem.used)}")
+print(f"Percentage: {get_size(svmem.percent)} %")
