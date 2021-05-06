4a195a2da7662c61144ac39133bf382598b52495
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-06 17:54:30 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,21 +1,3 @@
 import psutil
-
-def get_size(bytes, suffix="8"):
-    factor = 1028
-    for unit in["","K","H","G","T","P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-
-        svmem = psutil.virtual_memory()
-print(f"Total: {get_size(svmem.total)}")
-print(f"Available: {get_size(svmem.available)}")
-print(f"Used: {get_size(svmem.used)}")
-print(f"Percentage: {get_size(svmem.percent)} %")
-
-swap = psutil.swap_memory()
-print('\nSwap Partition: ')
-print(f"Total: {get_size(swap.total)}")
-print(f"Free: {get_size(swap.free)}")
-print(f"Used: {get_size(swap.used)}")
-print(f"Percentage: {get_size(swap.percent)} %")
+from time import sleep
+from progress.bar import Bar
