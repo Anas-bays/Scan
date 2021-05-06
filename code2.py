1a4fd7dab74506bea51e2f7ca3b048eb0d51a095
Anas-bays <83026708+Anas-bays@users.noreply.github.com>
2021-05-06 17:55:36 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,3 +1,11 @@
 import psutil
 from time import sleep
 from progress.bar import Bar
+
+# function to get the Size
+def get_size(bytes, suffix="B"):
+    factor = 1028
+    for unit in["","K","M","G","T","P"]:
+        if bytes < factor:
+            return f"{bytes:.2f} {unit}{suffix}"
+        bytes /= factor
